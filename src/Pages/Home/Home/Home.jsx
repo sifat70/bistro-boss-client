20205477c2659db44cb49b1f9392271987c742dd
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-11-13 10:13:18 +0600

    category fetured banner added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import PopularMenu from "../PopularMenu/PopularMenu";
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
             {/* <h1>this is home page</h1> */}
             <Banner></Banner>
             <Category></Category>
+            <PopularMenu></PopularMenu>
         </div>
     )
 };
