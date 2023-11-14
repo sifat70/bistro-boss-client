899ddc1543e8725e55201aac98dbdc6956e3c5bb
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-11-14 09:39:12 +0600

    menu cover menu categoey added

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
 
     const navOptions = <>
-        <li><a>Item 1</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/menu">Our Menu</Link></li>
         <li><a>Item 3</a></li>
     </>
     return (
