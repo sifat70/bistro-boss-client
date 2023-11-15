8958091504b068264b582e8ddf59d60f6da12412
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-11-15 09:46:43 +0600

    order tab added

---
+++ src/Order/Order/Order.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import Cover from '../../Pages/Shared/Cover/Cover';
+import orderCoverImg from '../../assets/shop/banner2.jpg'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import useMenu from '../../hooks/useMenu';
+import FoodCard from '../../components/FoodCard/FoodCard';
+import OrderTab from '../OrderTab/OrderTab';
+
+
+
+const Order = () => {
+    const [tabIndex, setTabIndex] = useState(0);
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert')
+    const soup = menu.filter(item => item.category === 'soup')
+    const salad = menu.filter(item => item.category === 'salad')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const offered = menu.filter(item => item.category === 'offered')
+
+
+    return (
+        <div>
+            <Cover img={orderCoverImg} title="Order Food"></Cover>
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList>
+                    <Tab>Salad</Tab>
+                    <Tab>Pizza</Tab>
+                    <Tab>Soup</Tab>
+                    <Tab>Dessert</Tab>
+                    <Tab>Drinks</Tab>
+                </TabList>
+                <TabPanel>
+                    <OrderTab items={salad}></OrderTab>
+                </TabPanel>
+                <TabPanel>
+                    <OrderTab items={desserts}></OrderTab>
+                </TabPanel>
+                <TabPanel>
+                    <OrderTab items={soup}></OrderTab>
+                </TabPanel>
+                <TabPanel>
+                    <OrderTab items={pizza}></OrderTab>
+                </TabPanel>
+                <TabPanel>
+                    <OrderTab items={offered}></OrderTab>
+                </TabPanel>
+            </Tabs>
+        </div>
+    );
+};
+
+export default Order;
--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,18 +1,12 @@
-import { useEffect, useState } from "react";
+
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([])
-    useEffect(() => {
-        fetch('menu.json')
-            .then(res => res.json())
-            .then(data => {
-                const popularItems = data.filter(item => item.category === 'popular');
-                setMenu(popularItems)
-            })
-    }, [])
+    const [menu] = useMenu();
+    const popular = menu.filter(item => item.category === 'popular')
     return (
         <div>
             <section className="mb-12">
@@ -22,14 +16,14 @@ const PopularMenu = () => {
                 ></SectionTitle>
                 <div className="grid md:grid-cols-2 gap-4">
                     {
-                        menu.map(item => <MenuItem
+                        popular.map(item => <MenuItem
                             key={item._id}
                             item={item}
                         ></MenuItem>)
                     }
                 </div>
                 <div className="flex mt-8 justify-center">
-                <button className="btn btn-outline uppercase border-0 border-b-4 mt-4">View Full menu</button>
+                    <button className="btn btn-outline uppercase border-0 border-b-4 mt-4">View Full menu</button>
                 </div>
             </section>
         </div>
