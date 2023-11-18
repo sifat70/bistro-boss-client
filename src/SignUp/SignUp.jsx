b4d484019b8488f6b0a322d8a132de194fe17443
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-11-18 19:26:41 +0600

    modify login page

--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -1,10 +1,72 @@
-
+import { useContext } from "react";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../provider/AuthProvider";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+
+    const { register, handleSubmit, formState: { errors }, } = useForm();
+    const {createUser} = useContext(AuthContext)
+
+    const onSubmit = data => {
+        console.log(data);
+
+        createUser(data.email, data.password)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser)
+        })
+    }
+
+
     return (
-        <div>
-            <h2>Sign Up</h2>
-        </div>
+        <>
+            <Helmet>
+                <title>Bistro Boss || Sign Up</title>
+            </Helmet>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Sign Up now!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name="name" {...register("name", { required: true })} placeholder="Input your name" className="input input-bordered" />
+                                {errors.name && <span className="text-red-500"> name is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name="email" {...register("email", { required: true })} placeholder="email" className="input input-bordered" />
+                                {errors.name && <span className="text-red-500"> email is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name="password" {...register("password", { required: true, minLength: 6, maxLength: 20, })} placeholder="password" className="input input-bordered" />
+                                {/* pattern: /[a-zA-Z][a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/ password validation */}
+                                {errors.password?.type === 'required' && <span className="text-red-500"> password required</span>}
+                                {errors.password?.type === 'minLength' && <span className="text-red-500"> password must be 6 characters</span>}
+                                {errors.password?.type === 'maxLength' && <span className="text-red-500"> password must be less than 20 characters</span>}
+                                {errors.password?.type === 'pattern' && <span className="text-red-500"> password must have one uppercase one lower case one number less than 20 characters</span>}
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className="btn btn-primary" type="submit" value="Sign Up" />
+                            </div>
+                        </form>
+                        <p><small>Already have an account? <Link to="/login">Login</Link></small></p>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
