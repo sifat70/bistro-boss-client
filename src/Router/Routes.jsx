aa8de5093acef370576ad2e83f8bf71cbe8b96a3
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-11-23 10:53:39 +0600

    payment method added

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -74,7 +74,8 @@ export const router = createBrowserRouter([
       },
       {
         path: "updateItem/:id",
-        element: <AdminRoute><UpdateItem></UpdateItem></AdminRoute>
+        element: <AdminRoute><UpdateItem></UpdateItem></AdminRoute>,
+        loader: ({params}) => fetch(`http://localhost:5000/menu/${params.id}`)
       }
     ]
   }
